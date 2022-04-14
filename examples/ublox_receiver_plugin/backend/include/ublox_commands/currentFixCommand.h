a83c53feb2647f4175ddb614d11b9c8c6c0eed5c
Taylor  Grant <taylorgrant@Taylors-MacBook-Pro.local>
2022-04-14 10:02:49 -0400

    removed comments

--- examples/ublox_receiver_plugin/backend/include/ublox_commands/currentFixCommand.h
+++ examples/ublox_receiver_plugin/backend/include/ublox_commands/currentFixCommand.h
@@ -9,33 +9,25 @@ class ReceiverGetFixCommand: public Command{
         ~ReceiverGetFixCommand(){};
         static void NavigationStatusCallbackIsFixed(ublox::NavStatus &nav_status, double &time_stamp){
             try{
-                std::cout << "GPS Fix Type: "<< std::endl;
                 if (nav_status.fixtype == 0x00) {
-                    std::cout << "No Fix" << std::endl;
                     ReceiverGetFixCommand::receiverStatus = ReceiverStatus::NO_FIX;
                     ReceiverGetFixCommand::receiverFix = true;
                 } else if (nav_status.fixtype == 0x01) {
-                    std::cout << "Dead Reckoning Only" << std::endl;
                     ReceiverGetFixCommand::receiverStatus = ReceiverStatus::FIX_1D;
                     ReceiverGetFixCommand::receiverFix = true;
                 } else if (nav_status.fixtype == 0x02) {
-                    std::cout << "2D Fix" << std::endl;
                     ReceiverGetFixCommand::receiverStatus = ReceiverStatus::FIX_2D;
                     ReceiverGetFixCommand::receiverFix = true;
                 } else if (nav_status.fixtype == 0x03) {
-                    std::cout << "3D Fix" << std::endl;
                     ReceiverGetFixCommand::receiverStatus = ReceiverStatus::FIX_3D;
                     ReceiverGetFixCommand::receiverFix = true;
                 } else if (nav_status.fixtype == 0x04) {
-                    std::cout << "GPS + Dead Reckoning" << std::endl;
                     ReceiverGetFixCommand::receiverStatus = ReceiverStatus::GPS_AND_DEAD_RECKONING;
                     ReceiverGetFixCommand::receiverFix = true;
                 } else if (nav_status.fixtype == 0x05) {
-                    std::cout << "Time Only" << std::endl;
                     ReceiverGetFixCommand::receiverStatus = ReceiverStatus::TIME_ONLY;
                     ReceiverGetFixCommand::receiverFix = true;
                 } else {
-                    std::cout << "Unhandled Fix Type Returned" << std::endl;
                     ReceiverGetFixCommand::receiverStatus = ReceiverStatus::NO_FIX;
                     ReceiverGetFixCommand::receiverFix = true;
                 }
--- examples/ublox_receiver_plugin/backend/src/commands/CreateUbloxReceiver.cpp
+++ examples/ublox_receiver_plugin/backend/src/commands/CreateUbloxReceiver.cpp
@@ -5,30 +5,30 @@
 
 CreateUbloxReceiverCommand::~CreateUbloxReceiverCommand(){}
 ublox::Ublox* CreateUbloxReceiverCommand::execute(){
-    std::string port = "";
-    for (auto & p : serial::list_ports()) {
-        // check if vendor id is ublox (1546) and product id is a receiver (begins with 01a)
-        // TODO: should we only support the newest ublox receiver (which we have) ... 01a8
-        if (p.hardware_id.find("VID:PID=1546:01a") != std::string::npos) {
-            port = p.port;
-            break;
-        }
-    }
+    std::string port = "/dev/tty.usbmodem2101";
+    // for (auto & p : serial::list_ports()) {
+    //     // check if vendor id is ublox (1546) and product id is a receiver (begins with 01a)
+    //     // TODO: should we only support the newest ublox receiver (which we have) ... 01a8
+    //     if (p.hardware_id.find("VID:PID=1546:01a") != std::string::npos) {
+    //         port = p.port;
+    //         break;
+    //     }
+    // }
 
-    if (port == "") {
-        std::cout << "Could not find port with ublox receiver." << std::endl;
-        return receiver;
-    }
+    // if (port == "") {
+    //     std::cout << "Could not find port with ublox receiver." << std::endl;
+    //     return receiver;
+    // }
 
     bool result = 0;
     while(result == 0){
         result = receiver->Connect(port,baudrate);
         if (result) {
-            std::cout << "Successfully connected to ublox receiver at " << port << std::endl;
+            // std::cout << "Successfully connected to ublox receiver at " << port << std::endl;
             return receiver;
         }
-        else {
-            std::cout << "Failed to connect to ublox receiver at " << port << std::endl;
-        }
+        // else {
+        //     std::cout << "Failed to connect to ublox receiver at " << port << std::endl;
+        // }
     }
 }
